b4fc59f67a6224409e4c4e08885c2fa8d8dd7c5f
jaehwan jung <seanjung0915@gmail.com>
2023-09-10 17:02:22 +0900

    패키지 변경

--- src/NullPointerExceptionExample.java
+++
@@ -1,9 +0,0 @@
-public class NullPointerExceptionExample {
-    public static void main(String[] args) {
-        int[] intArray=null;
-        intArray[0]=10;
-        String str=null;
-        System.out.println("총 문자수: "+str.length());
-
-    }
-}
