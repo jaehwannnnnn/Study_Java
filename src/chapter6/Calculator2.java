073319fd5ea2657978e26d37e96ef6df4da622c7
jaehwan jung <seanjung0915@gmail.com>
2023-09-14 13:27:31 +0900

    chapter6_2

---
+++ src/chapter6/Calculator2.java
@@ -0,0 +1,12 @@
+package chapter6;
+
+public class Calculator2 {
+    static double pi = 3.141592;
+    static int plus(int x,int y) {
+        return x+y;
+    }
+
+    static int minus(int x, int y) {
+        return x-y;
+    }
+}
