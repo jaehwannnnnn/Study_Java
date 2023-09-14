e66e7e4c9eed19548a6c621134afb5c609455747
jaehwan jung <seanjung0915@gmail.com>
2023-09-14 15:23:30 +0900

    chapter6_4

---
+++ src/chapter6/hyundai/Car.java
@@ -0,0 +1,12 @@
+package chapter6.hyundai;
+import chapter6.hankook.SnowTire;
+import chapter6.kumho.AllSeasonTire;
+
+
+public class Car {
+    chapter6.hankook.Tire tire1 = new chapter6.hankook.Tire();
+    chapter6.kumho.Tire tire2 = new chapter6.kumho.Tire();
+    SnowTire tire3 = new SnowTire();
+    AllSeasonTire tire4 = new AllSeasonTire();
+
+}
---
+++ src/chapter6/package2/C.java
@@ -0,0 +1,6 @@
+package chapter6.package2;
+
+public class C {
+    A a; //컴파일 에러
+    B b;
+}
