b64eab92742660eeba5c8fbd687817c187b6b956
jaehwannnnnn <saenjung0915@gmail.com>
2023-09-16 21:13:44 +0900

    chapter7_8

---
+++ src/chapter7/Phone3Example.java
@@ -0,0 +1,11 @@
+package chapter7;
+
+public class Phone3Example {
+    public static void main(String[] args) {
+        SmartPhone3 smartPhone = new SmartPhone3("홍길동");
+
+        smartPhone.turnOn();
+        smartPhone.internetSearch();
+        smartPhone.turnOff();
+    }
+}
---
+++ src/chapter7/SmartPhone3.java
@@ -0,0 +1,11 @@
+package chapter7;
+
+public class SmartPhone3 extends Phone3{
+    SmartPhone3(String owner) {
+        super(owner);
+    }
+
+    void internetSearch() {
+        System.out.println("인터넷 검색을 합니다.");
+    }
+}
