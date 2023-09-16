ad547ed3b12a17453a34789f989c305827210a86
jaehwannnnnn <saenjung0915@gmail.com>
2023-09-16 21:41:58 +0900

    chapter7_10

---
+++ src/chapter7/Employee.java
@@ -0,0 +1,7 @@
+package chapter7;
+
+public final class Employee extends Person2{
+    public void work() {
+        System.out.println("제품을 생산합니다.");
+    }
+}
---
+++ src/chapter7/Manager.java
@@ -0,0 +1,7 @@
+package chapter7;
+
+public non-sealed class Manager extends Person2 {
+    public void work() {
+        System.out.println("생산 관리를 합니다.");
+    }
+}
