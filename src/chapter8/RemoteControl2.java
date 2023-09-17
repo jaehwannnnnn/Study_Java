80285bb532568735bec5d63f447cb0e8bcd3b555
jaehwannnnnn <seanjung0915@gmail.com>
2023-09-17 19:14:31 +0900

    chapter8-1

--- src/chapter8/RemoteControl2.java
+++ src/chapter8/RemoteControl2.java
@@ -3,4 +3,10 @@ package chapter8;
 public interface RemoteControl2 {
     int MAX_VOLUME = 10;
     int MIN_VOLUME = 0;
+
+    void turnOn();
+
+    void turnOff();
+
+    void setVolume(int volume);
 }
