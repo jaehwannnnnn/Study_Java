2eb85e9b9d894924ac026f0c7986dee298c18b7c
jaehwannnnnn <saenjung0915@gmail.com>
2023-09-17 18:38:46 +0900

    chapter8

---
+++ src/chapter8/RemoteControlExample.java
@@ -0,0 +1,9 @@
+package chapter8;
+
+public class RemoteControlExample {
+    public static void main(String[] args) {
+        RemoteControl rc;
+        rc = new Television();
+        rc.turnOn();
+    }
+}
---
+++ src/chapter8/Television.java
@@ -0,0 +1,7 @@
+package chapter8;
+
+public class Television implements RemoteControl {
+    public void turnOn() {
+        System.out.println("TV를 켭니다.");
+    }
+}
