3b5aec4f8e4dc597b9c48e3d125edc1036a509fd
jaehwannnnnn <saenjung0915@gmail.com>
2023-09-17 18:48:43 +0900

    chapter8

---
+++ src/chapter8/RemoteControl2.java
@@ -0,0 +1,6 @@
+package chapter8;
+
+public interface RemoteControl2 {
+    int MAX_VOLUME = 10;
+    int MIN_VOLUME = 0;
+}
---
+++ src/chapter8/RemoteControl2Example.java
@@ -0,0 +1,8 @@
+package chapter8;
+
+public class RemoteControl2Example {
+    public static void main(String[] args) {
+        System.out.println("리모콘 최대 볼륨: "+RemoteControl2.MAX_VOLUME);
+        System.out.println("리모컨 최저 볼륨: "+RemoteControl2.MIN_VOLUME);
+    }
+}
