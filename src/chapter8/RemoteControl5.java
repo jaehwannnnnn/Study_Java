6fb39cb34c7eecd62a2928b69bfdc739eb9516e3
jaehwannnnnn <seanjung0915@gmail.com>
2023-09-19 20:48:57 +0900

    chapter8-5

---
+++ src/chapter8/RemoteControl5.java
@@ -0,0 +1,7 @@
+package chapter8;
+
+public interface RemoteControl5 {
+    void turnOn();
+
+    void turnOff();
+}
---
+++ src/chapter8/SmartTelevision.java
@@ -0,0 +1,15 @@
+package chapter8;
+
+public class SmartTelevision implements RemoteControl5, Searchable {
+    public void turnOn() {
+        System.out.println("TV를 켭니다.");
+    }
+
+    public void turnOff() {
+        System.out.println("TV를 끕니다");
+    }
+
+    public void search(String url) {
+        System.out.println(url + "을 검색합니다.");
+    }
+}
