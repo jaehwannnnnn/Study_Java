98ecdcc853dcbadfd36f245e99d0f29d7656bc67
jaehwannnnnn <seanjung0915@gmail.com>
2023-10-02 00:43:02 +0900

    chapter11-6

---
+++ src/chapter11/section6/InsufficientException.java
@@ -0,0 +1,9 @@
+package chapter11.section6;
+
+public class InsufficientException extends Exception {
+    public InsufficientException() {
+    }
+    public InsufficientException(String message) {
+        super(message);
+    }
+}
