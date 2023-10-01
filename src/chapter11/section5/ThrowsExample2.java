f75f08d58fbc83ded4bf902c403b8d565552d15e
jaehwannnnnn <seanjung0915@gmail.com>
2023-10-02 00:26:29 +0900

    chapter11-5

---
+++ src/chapter11/section5/ThrowsExample2.java
@@ -0,0 +1,10 @@
+package chapter11.section5;
+
+public class ThrowsExample2 {
+    public static void main(String[] args) throws Exception{
+        findClass();
+    }
+    public static void findClass() throws ClassNotFoundException {
+        Class.forName("java.lang.String2");
+    }
+}
