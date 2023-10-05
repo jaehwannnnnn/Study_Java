9c96ffd91f69c5f5af0780ba245ce028057f6341
jaehwannnnnn <seanjung0915@gmail.com>
2023-10-05 23:59:22 +0900

    chapter12-1

---
+++ src/chapter12/section3/example2/Student.java
@@ -0,0 +1,17 @@
+package chapter12.section3.example2;
+
+public class Student {
+    private int no;
+    private String name;
+
+    public Student(int no, String name) {
+        this.no= no;
+        this.name = name;
+    }
+
+    public int getNo(){return no;}
+
+    public String getName() {
+        return name;
+    }
+}
