6049643c848429f8b8394c16f7556ce3fb1deeb0
jaehwannnnnn <seanjung0915@gmail.com>
2023-10-11 23:13:33 +0900

    chapter14

---
+++ src/chapter14/section3/BeepPrintExample.java
@@ -0,0 +1,30 @@
+package chapter14.section3;
+
+import java.awt.*;
+
+public class BeepPrintExample {
+    public static void main(String[] args) {
+        Thread thread=new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Toolkit toolkit = Toolkit.getDefaultToolkit();
+                for (int i = 0; i < 5; i++) {
+                    toolkit.beep();
+                    try {
+                        Thread.sleep(500);
+                    } catch (Exception e) {
+                    }
+                }
+            }
+        });
+        thread.start();
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("띵");
+        }
+        try {
+            Thread.sleep(500);
+        } catch (Exception e) {
+        }
+    }
+}
---
+++ src/chapter14/section5/example3/YieldExample.java
@@ -0,0 +1,23 @@
+package chapter14.section5.example3;
+
+public class YieldExample {
+    public static void main(String[] args) {
+        WorkThread workThreadA = new WorkThread("workThreadA");
+        WorkThread workThreadB = new WorkThread("workThreadB");
+        workThreadA.start();
+        workThreadB.start();
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+        }
+        workThreadA.work = false;
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+        }
+        workThreadA.work = true;
+
+    }
+}
