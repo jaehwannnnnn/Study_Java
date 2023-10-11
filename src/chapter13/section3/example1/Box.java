10d4e262fd5d813b0167e7ec2cc578d96dd28472
jaehwannnnnn <seanjung0915@gmail.com>
2023-10-11 17:55:23 +0900

    chapter13-1

---
+++ src/chapter13/section3/example1/Box.java
@@ -0,0 +1,13 @@
+package chapter13.section3.example1;
+
+public class Box<T> {
+    private T t;
+
+    public T get() {
+        return t;
+    }
+
+    public void set(T t) {
+        this.t = t;
+    }
+}
---
+++ src/chapter13/section5/Person.java
@@ -0,0 +1,16 @@
+package chapter13.section5;
+
+public class Person {
+}
+
+class Worker extends Person {
+}
+
+class Student extends Person {
+}
+
+class HighStudent extends Student {
+}
+
+class MiddleStudent extends Student{
+}
