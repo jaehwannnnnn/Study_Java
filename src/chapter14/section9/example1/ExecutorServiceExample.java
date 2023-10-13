f2ba601c2cef8deaa1c9a29e29ed68b3f2b3a80e
jaehwannnnnn <seanjung0915@gmail.com>
2023-10-13 23:26:05 +0900

    chapter14-4

---
+++ src/chapter14/section9/example1/ExecutorServiceExample.java
@@ -0,0 +1,11 @@
+package chapter14.section9.example1;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ExecutorServiceExample {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        executorService.shutdownNow();
+    }
+}
