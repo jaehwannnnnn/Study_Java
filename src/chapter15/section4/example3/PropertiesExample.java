764d59609f99e6df0b0a7fdad0234112b2d87846
jaehwannnnnn <seanjung0915@gmail.com>
2023-10-17 17:18:18 +0900

    chapter15-6

---
+++ src/chapter15/section4/example3/PropertiesExample.java
@@ -0,0 +1,23 @@
+package chapter15.section4.example3;
+
+import java.util.Properties;
+
+public class PropertiesExample {
+    public static void main(String[] args) throws Exception {
+        Properties properties = new Properties();
+
+        properties.load(PropertiesExample.class.getResourceAsStream("database.properties"));
+
+        String driver = properties.getProperty("driver");
+        String url = properties.getProperty("url");
+        String username = properties.getProperty("username");
+        String password = properties.getProperty("password");
+        String admin = properties.getProperty("admin");
+
+        System.out.println("driver : " + driver);
+        System.out.println("url : " + url);
+        System.out.println("username : " + username);
+        System.out.println("password : " + password);
+        System.out.println("admin : " + admin);
+    }
+}
