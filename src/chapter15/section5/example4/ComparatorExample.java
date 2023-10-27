ed3b2d6d9061851e5e014d188f6c353757ebe7f5
jaehwannnnnn <seanjung0915@gmail.com>
2023-10-27 22:17:16 +0900

    chapter15-9

---
+++ src/chapter15/section5/example4/ComparatorExample.java
@@ -0,0 +1,17 @@
+package chapter15.section5.example4;
+
+import java.util.TreeSet;
+
+public class ComparatorExample {
+    public static void main(String[] args) {
+        TreeSet<Fruit> treeSet = new TreeSet<Fruit>(new FruitComparator());
+
+        treeSet.add(new Fruit("포도", 3000));
+        treeSet.add(new Fruit("수박", 10000));
+        treeSet.add(new Fruit("딸기", 6000));
+
+        for (Fruit fruit : treeSet) {
+            System.out.println(fruit.name + ":" + fruit.price);
+        }
+    }
+}
