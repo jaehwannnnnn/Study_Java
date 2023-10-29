f4c9f9e4490863c408b4c60d26f42baa3d1caa29
jaehwannnnnn <seanjung0915@gmail.com>
2023-10-29 17:26:49 +0900

    chapter15-11

---
+++ src/chapter15/section6/example2/Message.java
@@ -0,0 +1,11 @@
+package chapter15.section6.example2;
+
+public class Message {
+    public String command;
+    public String to;
+
+    public Message(String command, String to) {
+        this.command = command;
+        this.to = to;
+    }
+}
