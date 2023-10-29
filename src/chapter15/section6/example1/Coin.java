5ee90ed996c145480ccaebab0bcb92caecd5228a
jaehwannnnnn <seanjung0915@gmail.com>
2023-10-29 15:02:29 +0900

    chapter15-10

---
+++ src/chapter15/section6/example1/Coin.java
@@ -0,0 +1,13 @@
+package chapter15.section6.example1;
+
+public class Coin {
+    private int value;
+
+    public Coin(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
